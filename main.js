b50bfab163a89a1f571477ba4f99d7f89164c6dc
Jared Hanson <jaredhanson@gmail.com>
2013-06-20 14:37:19 -0700

    Stub out router implementation.

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 define(['exports', 'module',
+        './lib/router',
         './lib/route'],
-function(exports, module, Route) {
+function(exports, module, Router, Route) {
   
+  exports = module.exports = new Router();  
   exports.Route = Route;
   
 });
